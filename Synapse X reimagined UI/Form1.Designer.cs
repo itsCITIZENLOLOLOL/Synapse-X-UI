98b63cfd8006a5029b7c1aac0fda7c8ba8c57c86
itsCITIZENLOLOLOL <dowefoigeimou-1150@mail.berwie.com>
2024-05-09 12:59:50 +0200

    Add files via upload

--- Synapse X reimagined UI/Form1.Designer.cs
+++ Synapse X reimagined UI/Form1.Designer.cs
@@ -43,18 +43,18 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(368, 69);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Execute*NOT AVALIABLE*";
+            this.button1.Text = "Execute UR USING UI HAHA";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.DarkGray;
-            this.button2.Cursor = System.Windows.Forms.Cursors.No;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.Location = new System.Drawing.Point(399, 338);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(372, 69);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Inject*NOT AVALIABLE*";
+            this.button2.Text = "Inject";
             this.button2.UseVisualStyleBackColor = false;
             // 
             // panel1
@@ -76,7 +76,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(742, 269);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "--UPDATED!!";
+            this.textBox1.Text = "NOW 64BIT!";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
